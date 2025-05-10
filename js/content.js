022b00565d156abf32d18c817f85c30dbcd82aa3
Sudan Suwal <sudayn@Sudans-MacBook-Pro.local>
2025-05-10 20:08:38 +1000

    fix: unable to scroll after dialog is shown

--- js/content.js
+++ js/content.js
@@ -1,7 +1,54 @@
+const targetWords = [
+  "ad",
+  "blockers",
+  "violate",
+  "youtube's",
+  "terms",
+  "of",
+  "service",
+];
+
 function isAdPlaying() {
   // Basic check: YouTube shows a .ad-showing class on the player during ads
-  const player = document.querySelector('.html5-video-player');
-  return player?.classList.contains('ad-showing');
+  const player = document.querySelector(".html5-video-player");
+  return player?.classList.contains("ad-showing");
+}
+
+function isAdDialogPresent() {
+  const dialogs = document.querySelectorAll("tp-yt-paper-dialog");
+
+  const match = Array.from(dialogs).find((dialog) => {
+    const text = dialog.textContent?.toLowerCase() || "";
+    const matchedCount = targetWords.filter((word) =>
+      text.includes(word)
+    ).length;
+    return matchedCount / targetWords.length >= 0.5;
+  });
+
+  if (match && match.style.display !== "none") {
+    console.log("Matched dialog:");
+    return true;
+  } else {
+    return false;
+  }
+}
+
+function hideAdDialogs() {
+  const dialogs = document.querySelectorAll("tp-yt-paper-dialog");
+  dialogs.forEach((dialog) => {
+    dialog.style.display = "none";
+  });
+  const backdrops = document.querySelectorAll("tp-yt-iron-overlay-backdrop");
+  backdrops.forEach((backdrop) => {
+    backdrop.style.display = "none";
+  });
+
+  ["wheel", "touchmove", "scroll"].forEach((event) => {
+    window.addEventListener(event, (e) => e.stopPropagation(), {
+      capture: true,
+      passive: false,
+    });
+  });
 }
 
 function clickSkipButtonIfPresent() {
@@ -20,10 +67,14 @@ function clickSkipButtonIfPresent() {
 }
 
 function seekAdToEndIfPresent() {
-  const video = document.getElementsByClassName('video-stream')[0];
+  const video = document.getElementsByClassName("video-stream")[0];
   if (!video) return;
 
   console.log("isAd", isAdPlaying());
+
+  if (isAdDialogPresent()) {
+    hideAdDialogs();
+  }
 
   if (isAdPlaying()) {
     console.log("seeking to end of ad");
